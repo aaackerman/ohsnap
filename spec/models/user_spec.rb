3ab43490f2db5319214959337eb596eacd4e2e72
aaackerman <aaackerman@yahoo.com>
2014-08-29 10:22:47 -0400

    tests for users model written and passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+ it { is_expected.to validate_presence_of(:username) }
+ it { is_expected.to validate_uniqueness_of(:username) }
+
+ it { is_expected.to validate_presence_of(:first_name) }
+ it { is_expected.to validate_presence_of(:last_name) }
+
+ it { is_expected.to validate_presence_of(:email) }
+ it { is_expected.to validate_uniqueness_of(:email) }
+ it { is_expected.to validate_presence_of(:encrypted_password) }
+
+ it { is_expected.to have_many(:stories) }
+ it { is_expected.to have_many(:games) }
+
 end
