8da97149a8b8de1fe7312525a59e7d56d529ab7b
aaackerman <aaackerman@yahoo.com>
2014-09-02 21:46:45 -0400

    Working my profile and listed games

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,8 @@
-class UsersContoller < ApplicationController 
+class UsersController < ApplicationController 
 
   def show
     @user = User.find(params[:id])
   end
-
-  # def new
-  #   @user = User.new
-  # end
-
-  # def create
-  #   @user = User.new(user_params)
-  # end
-
-  # def edit 
-  #   @user = User.find(params[:id])
-  # end
-
-  # def update
-  #   @user = User.find(params[:id])
-  # end
-
-  # def destroy
-  #   @user = User.find(params[:id])
-  #   @user.destroy
-  #   redirect_to(root_path)
-  # end
 
   private
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get '/'               => 'welcome#index', as: 'root'
   get '/snapstories'   =>  'stories#index', as: 'story'
   get '/stories'        => 'stories#list'
-  get '/games'         => 'games#index'
   get '/play'           => 'games#play', as: 'play'
   get '/states'         =>  'states#index'
 
